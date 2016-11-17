80146b8d4996ec112120d0ea588bfcba3b00b187
lake <lakeinchina@qq.com>
2016-11-17 16:39:22 +0800

    init Decoder_mp42YUV

---
+++ Android/Decoder_mp42YUV/app/src/main/java/me/lake/decoder_mp42yuv/MainActivity.java
@@ -0,0 +1,13 @@
+package me.lake.decoder_mp42yuv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
